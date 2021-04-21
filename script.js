f4a3db244006f7d1f951b12af207e04af1e3a5ea
Artur Gręziak <alex4448@wp.pl>
2021-04-21 19:43:41 +0200

    commit 5

--- script.js
+++ script.js
@@ -9,30 +9,9 @@ formElement.addEventListener("submit", (e) => {
 
   let sumaZamowienia = 0;
 
-  // if (pizza === "pizzaMala")
-  // {
-  //   sumaZamowienia += 10.21;
-  // } else if (pizza === "pizzaSrednia")
-  // {
-  //   sumaZamowienia += 15.53;
-  // } else if (pizza === "pizzaDuza")
-  // {
-  //   sumaZamowienia += 20.91;
-  // }
   const cenaRozmiaruPizzy = [10, 15, 20];
 
   const cenaRozmiarusera = [5, 10, 15];
-
-  // if (ser === "serMaly")
-  // {
-  //   sumaZamowienia += 5.27;
-  // } else if (ser === "serSredni")
-  // {
-  //   sumaZamowienia += 10.53;
-  // } else if (ser === "serDuzy")
-  // {
-  //   sumaZamowienia += 15.91;
-  // }
 
   var skladniki = document.querySelectorAll('.skladniki input[type="checkbox"]');
   skladniki.forEach(function (el) {
