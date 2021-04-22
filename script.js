853e691db4e2af354735f94066a2c2c29e10515e
Artur Gręziak <alex4448@wp.pl>
2021-04-22 12:38:23 +0200

    commit 12

--- script.js
+++ script.js
@@ -29,6 +29,27 @@ formElement.addEventListener("submit", (e) => {
   //     sumaZamowienia += Number(el.value);
   // });
 
+  const ingredients = [
+    { name: "Cebula", price: 1 },
+    { name: "Szynka", price: 4 },
+    { name: "Pepperoni", price: 5 },
+    { name: "Chili", price: 7 },
+    { name: "Ananas", price: 6 },
+    { name: "Czosnek", price: 3 },
+    { name: "Krewetki", price: 8 },
+    { name: "Oliwki", price: 5 },
+    { name: "Rukola", price: 5 },
+    { name: " Kukurydza", price: 9 },
+    { name: "Łosoś", price: 16.23 },
+    { name: "Mozzarella", price: 3.90 },
+    { name: "Papryka", price: 6.23 },
+    { name: "Pomidor", price: 2.43 },
+    { name: "Pieczarki", price: 7.03 },
+    { name: "Cocacola", price: 6.50 },
+    { name: "Cocacola", price: 6.50 },
+    { name: "Keczup", price: 5.33 }
+  ];
+
   console.log(sumaZamowienia);
   
   
