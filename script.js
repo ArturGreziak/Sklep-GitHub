58d42312b16caf1da19dc3c58b153e1ef37702a2
Artur Gręziak <alex4448@wp.pl>
2021-04-24 11:02:53 +0200

    liquidation of a class in an object

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 let formElement = document.querySelector(".js-form");
 
 const ingredients = [
-  { value: "Cebula", price: 1, class_img: "towarC", src: "assets/cebula.png", alt: "Cebula", class_input: "jedzenie" },
-  { value: "Szynka", price: 4, class_img: "towarSZ", src: "assets/szynka.png", alt: "szynka", class_input: "jedzenie" },
-  { value: "Pepperoni", price: 5, class_img: "towarPE", src="assets/pepperoni.png", alt: "pepperoni", class_input: "jedzenie" },
-  { value: "Chili", price: 7, class_img: "chili", src: "assets/chili.png", alt: "chili", class_input: "jedzenie" },
-  { value: "Ananas", price: 6, class_img: "ananas", src: "assets/ananas.png", alt: "ananas", class_input: "jedzenie" },
-  { value: "Czosnek", price: 3, class_img: "czosnek", src: "assets/czosnek.png", alt: "czosnek", class_input: "jedzenie" },
-  { value: "Krewetki", price: 8, class_img: "krewetki", src: "assets/krewetki.png", alt: "krewetki", class_input: "jedzenie" },
-  { value: "Oliwki", price: 5, class_img: "oliwki", src: "assets/oliwki.png", alt: "oliwki", class_input: "jedzenie" },
-  { value: "Rukola", price: 5, class_img: "rukola", src: "assets/rukola.png", alt: "rukola", class_input: "jedzenie" },
-  { value: " Kukurydza", price: 9, class_img: "kukurydza", src: "assets/kukurydza.png", alt: "kukurydza", class_input: "jedzenie" },
-  { value: "Łosoś", price: 16.23, class_img: "łosoś", src: "assets/losos.png", alt: "łosoś", class_input: "jedzenie" },
+  { value: "Cebula", price: 1, class_img: "towarC", src: "assets/cebula.png", alt: "Cebula"},
+  { value: "Szynka", price: 4, class_img: "towarSZ", src: "assets/szynka.png", alt: "szynka"},
+  { value: "Pepperoni", price: 5, class_img: "towarPE", src="assets/pepperoni.png", alt: "pepperoni"},
+  { value: "Chili", price: 7, class_img: "chili", src: "assets/chili.png", alt: "chili"},
+  { value: "Ananas", price: 6, class_img: "ananas", src: "assets/ananas.png", alt: "ananas"},
+  { value: "Czosnek", price: 3, class_img: "czosnek", src: "assets/czosnek.png", alt: "czosnek"},
+  { value: "Krewetki", price: 8, class_img: "krewetki", src: "assets/krewetki.png", alt: "krewetki"},
+  { value: "Oliwki", price: 5, class_img: "oliwki", src: "assets/oliwki.png", alt: "oliwki"},
+  { value: "Rukola", price: 5, class_img: "rukola", src: "assets/rukola.png", alt: "rukola"},
+  { value: " Kukurydza", price: 9, class_img: "kukurydza", src: "assets/kukurydza.png", alt: "kukurydza"},
+  { value: "Łosoś", price: 16.23, class_img: "łosoś", src: "assets/losos.png", alt: "łosoś"},
   { value: "Mozzarella", price: 3.90, class_img: "mozzarella", src: "assets/mozzarella.png", alt: "mozzarella" },
-  { value: "Papryka", price: 6.23, class_img: "papryka", src: "assets/papryka.png", alt: "papryka", class_input: "jedzenie" },
-  { value: "Pomidor", price: 2.43, class_img: "pomidor", src: "assets/pomidor.png", alt: "pomidor", class_input: "jedzenie" },
-  { value: "Pieczarki", price: 7.03, class_img: "pieczarki", src: "assets/pieczarki.png", alt: "pieczarki", class_input: "jedzenie" },
-  { value: "Cocacola", price: 6.50, class_img: "white", src: "assets/white.png", alt: "white", class_input: "jedzenie" },
-  { value: "Cocacola", price: 6.50, class_img: "black", src: "assets/black.png", alt: "bleck", class_input: "jedzenie" },
-  { value: "Keczup", price: 5.33, class_img: "keczup", src: "assets/keczup.png", alt: "keczup", class_input: "jedzenie" }
+  { value: "Papryka", price: 6.23, class_img: "papryka", src: "assets/papryka.png", alt: "papryka"},
+  { value: "Pomidor", price: 2.43, class_img: "pomidor", src: "assets/pomidor.png", alt: "pomidor"},
+  { value: "Pieczarki", price: 7.03, class_img: "pieczarki", src: "assets/pieczarki.png", alt: "pieczarki"},
+  { value: "Cocacola", price: 6.50, class_img: "white", src: "assets/white.png", alt: "white"},
+  { value: "Cocacola", price: 6.50, class_img: "black", src: "assets/black.png", alt: "bleck"},
+  { value: "Keczup", price: 5.33, class_img: "keczup", src: "assets/keczup.png", alt: "keczup"}
 ];
 
 
